7fd2c7dffc18624ca60cd319d7cb027b27dccc63
Princess <princesstheaenriquez@gmail.com>
2021-08-22 14:20:42 -0500

    Added Nav Component.

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -3,6 +3,10 @@ import React from 'react';
 function Nav() {
 
     return (
-        
+        <header>
+            <h2>
+                <a href="/"></a>
+            </h2>
+        </header>
     )
 }
